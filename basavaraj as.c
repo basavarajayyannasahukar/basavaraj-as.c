cf2d60933f6587206b8d5a99a883d86afca6d81c
basavarajayyannasahukar <basavarajayyannasahukar@gmail.com>
2025-05-12 15:54:55 +0530

    Create basavaraj as.c

---
+++ basavaraj as.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int y,p,r,output;
+int main(){
+printf("enter the year\n");
+scanf("%d",&y);
+printf("enter the ammount\n");
+scanf("%d",&p);
+printf("enter the rate\n");
+scanf("%d",&r);
+return 0;
+}
