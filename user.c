147a77c8b7f2ebba1b8664cdc6f0318a61d2c79c
basavarajayyannasahukar <basavarajayyannasahukar@gmail.com>
2025-05-12 16:11:33 +0530

    Create user.c

---
+++ user.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int name,usn,age,output;
+int main(){
+printf("enter the name\n");
+scanf("%d",&name);
+printf("enter the usn\n");
+scanf("%d",&usn);
+printf("enter the age\n");
+scanf("%d",&age);
+return 0;
+}
