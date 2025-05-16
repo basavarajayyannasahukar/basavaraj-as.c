41313bd57d6664ffd4bc4fd64aadf8541be82064
basavarajayyannasahukar <basavarajayyannasahukar@gmail.com>
2025-05-16 12:34:43 +0530

    Create prime_number.c

---
+++ prime_number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int i,num,isprime=1;
+  printf("eneter the num");
+  scanf("%d",&num);
+  for(i=2;i<=num;i++);
+  if(i<=num/2){
+     isprime=0;}
+      else{
+          printf("its not a prime number");
+      }
+  }
